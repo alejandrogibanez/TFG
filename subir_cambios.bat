b9481f4ea6285b7bf1f89094319ffbc2eaf9ca67
Alex Garcia <alexgarciaibaez@gmail.com>
2026-05-28 19:50:31 +0200

    Commit automático: 28/05/2026 a las 19:50

--- subir_cambios.bat
+++ subir_cambios.bat
@@ -2,22 +2,26 @@
 :: Cambiar la codificación a UTF-8 para evitar problemas con tildes en los mensajes
 chcp 65001 > nul
 
-:: Obtener la fecha y hora actual del sistema
-set FECHA=%date% %time%
+:: 1. Capturar la hora actual y formatearla a HH:MM (gestionando el espacio de las mañanas)
+set HORA_LIMPIA=%time:~0,5%
+set HORA_LIMPIA=%HORA_LIMPIA: =0%
+
+:: 2. Unir la fecha y la hora limpia
+set FECHA=%date% a las %HORA_LIMPIA%
 
 echo === Iniciando subida de cambios a Git ===
 echo Fecha y hora actual: %FECHA%
 echo.
 
-:: 1. Añadir todos los cambios (nuevos y modificados)
+:: 3. Añadir todos los cambios (nuevos y modificados)
 echo [+] Añadiendo archivos...
 git add .
 
-:: 2. Hacer el commit con la fecha dinámica
+:: 4. Hacer el commit con la fecha y hora filtradas
 echo [+] Creando commit...
 git commit -m "Commit automático: %FECHA%"
 
-:: 3. Subir los cambios a la rama actual
+:: 5. Subir los cambios a la rama actual
 echo [+] Subiendo a GitHub/GitLab...
 git push
 
